0ef2b9392628cb8db2b2305af45f5e9c0905f235
rribeiro7 <mailpararegistodecontas@gmail.com>
2017-10-11 22:40:13 +0100

    v1 creation of util classes for connectivity and shared preferences

---
+++ app/src/main/java/pt/andreiaribeiro/com/andreiaribeiro/utils/Connectivity.java
@@ -0,0 +1,204 @@
+package pt.andreiaribeiro.com.andreiaribeiro.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.telephony.TelephonyManager;
+
+import java.io.IOException;
+
+/**
+ * Created by Rui on 27/09/2017.
+ * http://stackoverflow.com/questions/2802472/detect-network-connection-type-on-android/8548926#8548926
+ */
+
+public class Connectivity {
+    public static final String NOTCONNECTED = "notConected";
+    public static final String WIFI = "WIFI";
+    public static final String NET2G = "2G";
+    public static final String NET3G = "3G";
+    public static final String NET4G = "4G";
+    public static final String UNKNOWN = "?";
+
+    /**
+     * Get the network info
+     *
+     * @param context
+     * @return
+     */
+    public static NetworkInfo getNetworkInfo(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        return cm.getActiveNetworkInfo();
+    }
+
+    /**
+     * Check if there is any connectivity
+     *
+     * @param context
+     * @return
+     */
+    public static boolean isConnected(Context context) {
+        NetworkInfo info = Connectivity.getNetworkInfo(context);
+        return (info != null && info.isConnected());
+    }
+
+    /**
+     * Check if there is any connectivity to a Wifi network
+     *
+     * @param context
+     * @return
+     */
+    public static boolean isConnectedWifi(Context context) {
+        NetworkInfo info = Connectivity.getNetworkInfo(context);
+        return (info != null && info.isConnected() && info.getType() == ConnectivityManager.TYPE_WIFI);
+    }
+
+    /**
+     * Check if there is any connectivity to a mobile network
+     *
+     * @param context
+     * @return
+     */
+    public static boolean isConnectedMobile(Context context) {
+        NetworkInfo info = Connectivity.getNetworkInfo(context);
+        return (info != null && info.isConnected() && info.getType() == ConnectivityManager.TYPE_MOBILE);
+    }
+
+    /**
+     * Check if there is fast connectivity
+     *
+     * @param context
+     * @return
+     */
+    public static boolean isConnectedFast(Context context) {
+        NetworkInfo info = Connectivity.getNetworkInfo(context);
+        return (info != null && info.isConnected() && Connectivity.isConnectionFast(info.getType(), info.getSubtype()));
+    }
+
+    /**
+     * Check if the connection is fast
+     *
+     * @param type    type of conection, wifi or mobile sim
+     * @param subType subtype of conection, gprs, 2g, 3g...
+     * @return
+     */
+    public static boolean isConnectionFast(int type, int subType) {
+        if (type == ConnectivityManager.TYPE_WIFI) {
+            return true;
+        } else if (type == ConnectivityManager.TYPE_MOBILE) {
+            switch (subType) {
+                case TelephonyManager.NETWORK_TYPE_1xRTT:
+                    return false; // ~ 50-100 kbps
+                case TelephonyManager.NETWORK_TYPE_CDMA:
+                    return false; // ~ 14-64 kbps
+                case TelephonyManager.NETWORK_TYPE_EDGE:
+                    return false; // ~ 50-100 kbps
+                case TelephonyManager.NETWORK_TYPE_EVDO_0:
+                    return true; // ~ 400-1000 kbps
+                case TelephonyManager.NETWORK_TYPE_EVDO_A:
+                    return true; // ~ 600-1400 kbps
+                case TelephonyManager.NETWORK_TYPE_GPRS:
+                    return false; // ~ 100 kbps
+                case TelephonyManager.NETWORK_TYPE_HSDPA:
+                    return true; // ~ 2-14 Mbps
+                case TelephonyManager.NETWORK_TYPE_HSPA:
+                    return true; // ~ 700-1700 kbps
+                case TelephonyManager.NETWORK_TYPE_HSUPA:
+                    return true; // ~ 1-23 Mbps
+                case TelephonyManager.NETWORK_TYPE_UMTS:
+                    return true; // ~ 400-7000 kbps
+            /*
+             * Above API level 7, make sure to set android:targetSdkVersion
+             * to appropriate level to use these
+             */
+                case TelephonyManager.NETWORK_TYPE_EHRPD: // API level 11
+                    return true; // ~ 1-2 Mbps
+                case TelephonyManager.NETWORK_TYPE_EVDO_B: // API level 9
+                    return true; // ~ 5 Mbps
+                case TelephonyManager.NETWORK_TYPE_HSPAP: // API level 13
+                    return true; // ~ 10-20 Mbps
+                case TelephonyManager.NETWORK_TYPE_IDEN: // API level 8
+                    return false; // ~25 kbps
+                case TelephonyManager.NETWORK_TYPE_LTE: // API level 11
+                    return true; // ~ 10+ Mbps
+                // Unknown
+                case TelephonyManager.NETWORK_TYPE_UNKNOWN:
+                default:
+                    return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Check if its 2g, 3g or 4g (or unknown)
+     *
+     * @param context
+     * @return
+     */
+    public static String getNetworkClass(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo info = cm.getActiveNetworkInfo();
+        if (info == null || !info.isConnected())
+            return NOTCONNECTED;
+        if (info.getType() == ConnectivityManager.TYPE_WIFI)
+            return WIFI;
+        if (info.getType() == ConnectivityManager.TYPE_MOBILE) {
+            int networkType = info.getSubtype();
+            switch (networkType) {
+                case TelephonyManager.NETWORK_TYPE_GPRS:
+                case TelephonyManager.NETWORK_TYPE_EDGE:
+                case TelephonyManager.NETWORK_TYPE_CDMA:
+                case TelephonyManager.NETWORK_TYPE_1xRTT:
+                case TelephonyManager.NETWORK_TYPE_IDEN: //api<8 : replace by 11
+                    return NET2G;
+                case TelephonyManager.NETWORK_TYPE_UMTS:
+                case TelephonyManager.NETWORK_TYPE_EVDO_0:
+                case TelephonyManager.NETWORK_TYPE_EVDO_A:
+                case TelephonyManager.NETWORK_TYPE_HSDPA:
+                case TelephonyManager.NETWORK_TYPE_HSUPA:
+                case TelephonyManager.NETWORK_TYPE_HSPA:
+                case TelephonyManager.NETWORK_TYPE_EVDO_B: //api<9 : replace by 14
+                case TelephonyManager.NETWORK_TYPE_EHRPD:  //api<11 : replace by 12
+                case TelephonyManager.NETWORK_TYPE_HSPAP:  //api<13 : replace by 15
+                    return NET3G;
+                case TelephonyManager.NETWORK_TYPE_LTE:    //api<11 : replace by 13
+                    return NET4G;
+                default:
+                    return UNKNOWN;
+            }
+        }
+        return UNKNOWN;
+    }
+
+    /**
+     * Funcao que verifica a ligacao a internet
+     */
+    public static boolean isOnline(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isInternetWorking() {
+        Runtime runtime = Runtime.getRuntime();
+        try {
+
+            Process ipProcess = runtime.exec("/system/bin/ping -c 1 8.8.8.8");
+            int exitValue = ipProcess.waitFor();
+            return (exitValue == 0);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+}
