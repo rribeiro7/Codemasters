76ee577d6f36571c87f4abd43c59df015bb5a3d5
rribeiro7 <mailpararegistodecontas@gmail.com>
2017-12-03 15:37:46 +0000

    register v1

---
+++ app/src/main/java/pt/andreiaribeiro/com/andreiaribeiro/utils/StringUtils.java
@@ -0,0 +1,12 @@
+package pt.andreiaribeiro.com.andreiaribeiro.utils;
+
+/**
+ * Created by Rui on 03/12/2017.
+ */
+
+public class StringUtils {
+
+    public static boolean isNullOrEmpty(String s){
+        return s == null || s.trim().isEmpty();
+    }
+}
